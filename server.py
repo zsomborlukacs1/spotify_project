0f240f55db3b999cc7b0f4e7036cb97686c5b166
Lukács Zsombor <lukacs.zsombor@student.ms.sapientia.ro>
2024-02-17 12:18:15 +0200

    Auth and profile data

--- server.py
+++ server.py
@@ -1,10 +1,49 @@
-from flask import Flask
+from flask import Flask, redirect, request, jsonify, session
+from spotipy import Spotify, SpotifyOAuth
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+# Spotify API configuration
+SPOTIPY_CLIENT_ID = 'd7633c6c884e4a26ba105105422becea'
+SPOTIPY_CLIENT_SECRET = '03aa0e231e0e4e19af41dadebac8e55d'
+SPOTIPY_REDIRECT_URI = 'http://localhost:5000/callback'
 
+# Spotify OAuth object
+sp_oauth = SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
+                        client_secret=SPOTIPY_CLIENT_SECRET,
+                        redirect_uri=SPOTIPY_REDIRECT_URI,
+                        scope="user-library-read")
+
+# Login endpoint
+@app.route('/login')
+def index():
+    auth_url = sp_oauth.get_authorize_url()
+    return f'<a href="{auth_url}">Login with Spotify</a>'
+
+# Callback endpoint which gets the auth token and saves it to the session for later use
+@app.route('/callback')
+def callback():
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+
+    access_token = token_info['access_token']
+    session['access_token'] = access_token
+    return redirect('/profile')
+
+# Profile endpoint, with all the datas of the user (For the My Profile Page)
+@app.route('/profile')
+def get_user_data():
+    access_token = session.get('access_token')
+    if access_token:
+        sp = Spotify(auth=access_token)
+        try:
+            user_info = sp.current_user()
+            return jsonify(user_info)
+        except Exception as e:
+            return jsonify({'error': str(e)}), 500
+    else:
+        return jsonify({'error': 'Access token is missing.'}), 400
+    
 if __name__ == "__main__":
     app.run(debug=True)
